a3eb69c61876da9330886a18ec1dbc82c133ef25
Banovvv <ivan_banov@abv.bg>
2022-05-30 16:21:04 +0300

    Added basic Move logic to the Player class

--- Ship-Captain-Crew/Player.cs
+++ Ship-Captain-Crew/Player.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Ship_Captain_Crew
 {
     public class Player
     {
+        private bool ShipCaptainCrew = false;
+
         public Player()
         {
             Dice.Add(new Die());
@@ -15,6 +18,7 @@ namespace Ship_Captain_Crew
         }
 
         public string Name { get; set; }
+        public int Score { get; set; } = 0;
         public List<Die> Dice { get; set; } = new List<Die>();
         public List<Die> Bank { get; set; } = new List<Die>();
 
@@ -22,7 +26,47 @@ namespace Ship_Captain_Crew
         {
             for (int i = 0; i < 3; i++) // Every player has exactly three throws
             {
+                RollDice();
 
+                if (!ShipCaptainCrew)
+                {
+                    Die hasSix = Dice.Where(x => x.Score == 6).FirstOrDefault();
+                    Die hasFive = Dice.Where(x => x.Score == 5).FirstOrDefault();
+                    Die hasFour = Dice.Where(x => x.Score == 4).FirstOrDefault();
+
+                    if (hasSix != null || Bank.Count == 1)
+                    {
+                        if (Bank.Count == 0)
+                        {
+                            Bank.Add(hasSix);
+                            Dice.Remove(hasSix);
+                        }
+
+                        if (hasFive != null || Bank.Count == 2)
+                        {
+                            if (Bank.Count == 1)
+                            {
+                                Bank.Add(hasFive);
+                                Dice.Remove(hasFive);
+                            }
+
+                            if (hasFour != null)
+                            {
+                                Bank.Add(hasFour);
+                                Dice.Remove(hasFour);
+
+                                ShipCaptainCrew = true;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var die in Dice)
+                    {
+                        Score += die.Score;
+                    }
+                }
             }
         }
 
