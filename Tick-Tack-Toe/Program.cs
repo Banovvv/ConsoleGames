5098a03003c30696aaf49caf6decf0591338bda7
Banovvv <ivan_banov@abv.bg>
2022-07-11 15:23:22 +0300

    Added Tick-Tack-Toe

---
+++ Tick-Tack-Toe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tick_Tack_Toe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
