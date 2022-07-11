d6201a015d700036de7b7630235aeace495f0b74
Banovvv <ivan_banov@abv.bg>
2022-07-11 15:40:20 +0300

    Started working on the Board

---
+++ Tick-Tack-Toe/Models/Board.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tick_Tack_Toe.Interfaces;
+
+namespace Tick_Tack_Toe.Models
+{
+    public class Board : IBoard
+    {
+    }
+}
